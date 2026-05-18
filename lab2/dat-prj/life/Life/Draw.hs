eac034509d2b01909205bfaaea95e8ff8150363c
Elies <68759092+eliesgalvira@users.noreply.github.com>
2026-05-18 19:45:48 +0200

    Add DAT lab 2 project and bundled libraries
    - Add the `dat-prj` Stack/Cabal workspace
    - Vendor DAT framework and drawing dependencies
    - Include the Life exercise sources and bundled archive

---
+++ lab2/dat-prj/life/Life/Draw.hs
@@ -0,0 +1,24 @@
+
+module Life.Draw where
+import Life.Board
+import Drawing
+
+drawBoard :: Board -> Drawing
+drawBoard board =
+    -- A completar per l'estudiant
+    ...
+
+-- 'drawGrid minPos maxPos' obte el dibuix d'una graella que inclou els 2 extrems indicats.
+-- 'minPos' es la posicio de l'extrem (esquerra, inferior) i
+-- 'maxPos' es la posicio de l'extrem (dreta, superior)'.
+-- NOTA: Aquesta funcio s'usa a partir del segon pas de la practica.
+drawGrid :: Pos -> Pos -> Drawing
+drawGrid minPos maxPos =
+    colored gray $
+        foldMap vline [fst minPos .. fst maxPos + 1]
+        <> foldMap hline [snd minPos .. snd maxPos + 1]
+    where
+        hline row = polyline [ posToCoords (fst minPos, row), posToCoords (fst maxPos + 1, row) ]
+        vline col = polyline [ posToCoords (col, snd minPos), posToCoords (col, snd maxPos + 1) ]
+        posToCoords (col, row) = (fromIntegral col - 0.5, fromIntegral row - 0.5)
+
